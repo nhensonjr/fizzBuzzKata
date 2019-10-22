cceba55896d436397b26b6422bfd4189870227d9
NHENSON <norman.henson@vizientinc.com>
2019-10-22 17:03:44 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ConsoleOutputService } from './console-output.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fizzbuzz';
+
+  constructor(private outputService: ConsoleOutputService) {
+    this.outputService.fizzBuzzKata();
+  }
 }
---
+++ src/app/console-output.service.ts
@@ -0,0 +1,10 @@
+export class ConsoleOutputService {
+
+  fizzBuzzKata() {
+    // How to print to the console;
+    // console.log('Hello World!');
+
+    //    Code goes here    //
+    
+  }
+}
